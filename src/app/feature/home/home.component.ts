b48971ff4637fe9a8557d9ea37bd16c4924778c6
Fernando Vieira <fernandonvieira47@hotmail.com>
2024-06-25 17:36:13 -0300

    feat:add department options in home

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,5 +1,12 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
 import { PageOptionComponent } from './page-option/page-option.component';
+import { AuthService } from '../../core/auth/auth.service';
 
 interface IPageOption {
   title: string;
@@ -7,6 +14,33 @@ interface IPageOption {
   icon: string;
   link: string;
 }
+
+const OPTIONS = [
+  {
+    title: 'Novo Funcionário',
+    subtitle: 'Faça o cadastro de um novo funcionário',
+    icon: 'person_add',
+    link: '/app/employee/add',
+  },
+  {
+    title: 'Listar Funcionários',
+    subtitle: 'Busque por um funcionário na lista de funcionários',
+    icon: 'list_alt',
+    link: '/app/employee',
+  },
+  {
+    title: 'Novo Departamento',
+    subtitle: 'Faça o cadastro de um novo departamento',
+    icon: 'work',
+    link: '/app/department/add',
+  },
+  {
+    title: 'Listar Departamentos',
+    subtitle: 'Busque por um departamento na lista de departamentos',
+    icon: 'list_alt',
+    link: '/app/department',
+  },
+];
 
 @Component({
   selector: 'app-home',
@@ -17,18 +51,14 @@ interface IPageOption {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-  protected pageOptions = signal<IPageOption[]>([
-    {
-      title: 'Novo Funcionário',
-      subtitle: 'Faça o cadastro de um novo funcionário',
-      icon: 'person_add',
-      link: '/app/employee/add',
-    },
-    {
-      title: 'Listar Funcionários',
-      subtitle: 'Busque por um funcionário na lista de funcionários',
-      icon: 'list_alt',
-      link: '/app/employee',
-    },
-  ]);
+  private authService = inject(AuthService);
+  protected employee = this.authService.getEmployee();
+
+  protected pageOptions = computed(() => {
+    if (this.employee()?.typeEmployee === 'EMPLOYEE') {
+      return [...OPTIONS.slice(0, 2)];
+    }
+
+    return [...OPTIONS];
+  });
 }
